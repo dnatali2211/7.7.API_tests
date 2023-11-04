4ad9f3349fa163bb93e6dc91d978cffe7cefd6bf
Nataliia Bulatovskaia <dnatali2211@gmail.com>
2023-11-04 23:06:25 +0400

    Added Dashboard

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   e2e: {
+    projectId: "hxsfa7",
     baseUrl: "https://petstore.swagger.io/v2/swagger.json",
     setupNodeEvents(on, config) {
       // implement node event listeners here
